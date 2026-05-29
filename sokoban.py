b451ef805bf4479161451ba18b5285080ef4cc70
AustereTriceratops <gatewaylevi@gmail.com>
2026-05-29 00:20:14 -0700

    fixed error when calling apply_move

--- sokoban.py
+++ sokoban.py
@@ -177,74 +177,7 @@ def add_step_constraints(s: Solver,
     is_reachable, _, _ = reachable(n_z3, envs[i], x_positions[i], y_positions[i], opp, wall_is_stop[i], max_steps=r, meta_index=i)
     s.add(is_reachable)
     
-    m_len = Int(f'move_len_{i}')
-    s.add(And(m_len > 0, m_len < n_z3))
-    m_fa = Int(f'move_forall_{i}')
-    
-    ### moves
-    # 0: left
-    # 1: right
-    # 2: up
-    # 3: down
-    s.add(If(moves[i] == 0,
-        And(
-            dst_x == src_x - m_len, dst_x >= 0, dst_y == src_y,
-            opp_x == src_x + 1, opp_x < n_z3, opp_y == src_y,
-            ForAll([m_fa],
-                If(
-                    And(m_fa >= dst_x, m_fa < src_x),
-                    Select(envs[i], m_fa + n_z3*dst_y) == EMPTY,
-                    True
-                )
-            )
-        ),
-        True
-    ))
-    
-    s.add(If(moves[i] == 1,
-        And(
-            dst_x == src_x + m_len, dst_x < n_z3, dst_y == src_y, 
-            opp_x == src_x - 1, opp_x >= 0, opp_y == src_y,
-            ForAll([m_fa],
-                If(
-                    And(m_fa > src_x, m_fa <= dst_x),
-                    Select(envs[i], m_fa + n_z3*dst_y) == EMPTY,
-                    True
-                )
-            )
-        ),
-        True
-    ))
-    
-    s.add(If(moves[i] == 2,
-        And(
-            dst_y == src_y + m_len, dst_y < n_z3, dst_x == src_x,
-            opp_y == src_y - 1, opp_y >= 0, opp_x == src_x,
-            ForAll([m_fa],
-                If(
-                    And(m_fa > src_y, m_fa <= dst_y),
-                    Select(envs[i], dst_x + n_z3*m_fa) == EMPTY,
-                    True
-                )
-            )
-        ),
-        True
-    ))
-    
-    s.add(If(moves[i] == 3,
-        And(
-            dst_y == src_y - m_len, dst_y >= 0, dst_x == src_x,
-            opp_y == src_y + 1, opp_y < n_z3, opp_x == src_x,
-            ForAll([m_fa],
-                If(
-                    And(m_fa >= dst_y, m_fa < src_y),
-                    Select(envs[i], dst_x + n_z3*m_fa) == EMPTY,
-                    True
-                )
-            )
-        ),
-        True
-    ))
+    apply_move(s, i, n_z3, moves, envs, src_x, src_y, dst_x, dst_y, opp_x, opp_y, x_positions, y_positions)
 
     for j in range(n_sq):
         s.add(If(
@@ -255,5 +188,93 @@ def add_step_constraints(s: Solver,
     
     s.add(Select(envs[i+1], dst) == src_tile)
     s.add(Select(envs[i+1], src) == EMPTY)
-    s.add(x_positions[i+1] == src_x)
-    s.add(y_positions[i+1] == src_y)
+    
+
+### moves
+    # 0: left
+    # 1: right
+    # 2: up
+    # 3: down
+def apply_move(
+    s: Solver,
+    i: int,
+    n_z3: IntNumRef,
+    moves: List[IntNumRef],
+    envs: List[ArrayRef],
+    src_x, src_y, dst_x, dst_y, opp_x, opp_y, x_positions, y_positions
+):
+    m_len = Int(f'move_len_{i}')
+    s.add(And(m_len > 0, m_len < n_z3))
+    m_fa = Int(f'move_forall_{i}')
+    
+    m0_forall = ForAll([m_fa],
+        If(
+            And(m_fa >= dst_x, m_fa < src_x),
+            Select(envs[i], m_fa + n_z3*dst_y) == EMPTY,
+            True
+        )
+    )
+    
+    s.add(If(moves[i] == 0,
+        And(
+            dst_x == src_x - m_len, dst_x >= 0, dst_y == src_y,
+            opp_x == src_x + 1, opp_x < n_z3, opp_y == src_y,
+            y_positions[i+1] == dst_y, x_positions[i+1] == dst_x + 1,
+            m0_forall
+        ),
+        True
+    ))
+    
+    m1_forall = ForAll([m_fa],
+        If(
+            And(m_fa > src_x, m_fa <= dst_x),
+            Select(envs[i], m_fa + n_z3*dst_y) == EMPTY,
+            True
+        )
+    )
+    
+    s.add(If(moves[i] == 1,
+        And(
+            dst_x == src_x + m_len, dst_x < n_z3, dst_y == src_y, 
+            opp_x == src_x - 1, opp_x >= 0, opp_y == src_y,
+            y_positions[i+1] == dst_y, x_positions[i+1] == dst_x - 1,
+            m1_forall
+        ),
+        True
+    ))
+    
+    m2_forall = ForAll([m_fa],
+        If(
+            And(m_fa > src_y, m_fa <= dst_y),
+            Select(envs[i], dst_x + n_z3*m_fa) == EMPTY,
+            True
+        )
+    )
+    
+    s.add(If(moves[i] == 2,
+        And(
+            dst_y == src_y + m_len, dst_y < n_z3, dst_x == src_x,
+            opp_y == src_y - 1, opp_y >= 0, opp_x == src_x,
+            x_positions[i+1] == dst_x, y_positions[i+1] == dst_y - 1,
+            m2_forall
+        ),
+        True
+    ))
+    
+    m3_forall = ForAll([m_fa],
+        If(
+            And(m_fa >= dst_y, m_fa < src_y),
+            Select(envs[i], dst_x + n_z3*m_fa) == EMPTY,
+            True
+        )
+    )
+    
+    s.add(If(moves[i] == 3,
+        And(
+            dst_y == src_y - m_len, dst_y >= 0, dst_x == src_x,
+            opp_y == src_y + 1, opp_y < n_z3, opp_x == src_x,
+            x_positions[i+1] == dst_x, y_positions[i+1] == dst_y + 1,
+            m3_forall
+        ),
+        True
+    ))
